f0d5055fce4dd641789dae250954e6dd5619f9bf
Kristian Boda <kboda+git@protonmail.com>
2023-06-27 22:50:53 +0100

    refactor: add methods to data structures

--- src/api.py
+++ src/api.py
@@ -3,16 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Form, Request
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-from pomelo import (
-    addPlayer,
-    addScores,
-    createLeague,
-    deletePlayer,
-    leagueMatches,
-    leagueRanking,
-    listLeagues,
-    selectPlayers,
-)
+from structs import League, leagues
 
 templates = Jinja2Templates(directory="templates")
 api = APIRouter()
@@ -20,50 +11,48 @@ api = APIRouter()
 
 @api.get("/")
 async def home(request: Request):
-    leagues = listLeagues()
     return templates.TemplateResponse(
-        "index.html", {"request": request, "leagues": leagues}
+        "index.html", {"request": request, "leagues": leagues()}
     )
 
 
 @api.post("/create")
-async def create(league: Annotated[str, Form()]):
-    createLeague(league)
+async def create(league_id: Annotated[str, Form()]):
+    League.from_id(league_id)
     return RedirectResponse("/", status_code=303)
 
 
 @api.get("/league")
-async def select_league(id: str):
-    return RedirectResponse(f"/{id}", status_code=303)
+async def league(league_id: str):
+    return RedirectResponse(f"/{league_id}", status_code=303)
 
 
 @api.get("/{league_id}")
-async def league_get(request: Request, league_id: str):
-    players = selectPlayers(league_id)
-    ranking = leagueRanking(league_id)
-    matches = leagueMatches(league_id)
+async def get_league(request: Request, league_id: str):
+    league = League.from_id(league_id)
     return templates.TemplateResponse(
         "tournament_index.html",
         {
             "request": request,
             "league": league_id,
-            "players": players,
-            "matches": matches,
-            "ranking_stable": ranking["stable"],
-            "ranking_unstable": ranking["unstable"],
+            "players": league.get_players(),
+            "matches": league.get_matches(),
+            "ranking": league.get_ranking(),
         },
     )
 
 
 @api.post("/{league_id}/player")
 async def add_player(league_id: str, player: Annotated[str, Form()]):
-    addPlayer(league_id=league_id, name=player)
+    league = League.from_id(league_id)
+    league.add_player(name=player)
     return RedirectResponse(f"/{league_id}", status_code=303)
 
 
 @api.post("/{league_id}/delete_player")
 async def delete_player(league_id: str, player: Annotated[str, Form()]):
-    deletePlayer(league_id=league_id, name=player)
+    league = League.from_id(league_id)
+    league.delete_player(name=player)
     return RedirectResponse(f"/{league_id}", status_code=303)
 
 
@@ -75,5 +64,6 @@ async def score(
     player2: Annotated[str, Form()],
     player2_score: Annotated[int, Form()],
 ):
-    addScores(league_id, player1, player1_score, player2, player2_score)
+    league = League.from_id(league_id)
+    league.add_match(player1, player1_score, player2, player2_score)
     return RedirectResponse(f"/{league_id}", status_code=303)
--- src/structs.py
+++ src/structs.py
@@ -1,10 +1,18 @@
 import json
+import os
+import time
 
+import trueskill
 from pydantic import BaseModel
+
+data_dir = os.path.dirname("r/")
+
+def leagues():
+    return [f[:-5] for f in os.listdir(data_dir)]
 
 
 class Player(BaseModel):
-    rank: int = 1000
+    rank: int
     match_count: int = 0
 
 
@@ -21,5 +29,96 @@ class League(BaseModel):
         data = json.load(open(path))
         return League(**data)
 
+    @staticmethod
+    def from_id(league_id):
+        file_path = f"{data_dir}/{league_id}.json"
+        if not os.path.exists(file_path):
+            league = League(name=league_id, path=file_path)
+            league.to_json()
+            return league
+        else:
+            data = json.load(open(file_path))
+            trueskill.setup(data["trueskill"], data["trueskill"] / 3)
+            return League(**data)
+
     def to_json(self):
         json.dump(self.dict(), open(self.path, "w"), indent=2)
+
+    def add_player(self, name):
+        if name not in self.players:
+            self.players[name] = Player(rank=self.trueskill)
+        self.to_json()
+
+    def delete_player(self, name):
+        if name in self.players:
+            del self.players[name]
+        self.to_json()
+
+    def get_players(self):
+        return sorted(self.players.keys())
+
+    def get_matches(self):
+        return [(m[0], m[1], m[3], m[2], m[4]) for m in self.matches]
+
+    def get_ranking(self):
+        ranking = {"stable": [], "unstable": []}
+
+        for name, values in self.players.items():
+            if values.match_count >= self.min_games:
+                ranking["stable"].append(
+                    (name, values.rank, values.match_count)
+                )
+            else:
+                ranking["unstable"].append(
+                    (name, values.rank, values.match_count)
+                )
+        ranking["stable"] = sorted(
+            ranking["stable"],
+            key=lambda player: (player[1], player[2]),
+            reverse=True,
+        )
+        ranking["unstable"] = sorted(
+            ranking["unstable"],
+            key=lambda player: (player[1], player[2]),
+            reverse=True,
+        )
+        return ranking
+
+    def add_match(self, player1, player1_score, player2, player2_score):
+        now = time.localtime()
+        datetime = (
+            str(now[3])
+            + ":"
+            + str(now[4])
+            + " - "
+            + str(now[2])
+            + "/"
+            + str(now[1])
+        )
+        self.matches.append(
+            (
+                player1,
+                player1_score,
+                player2,
+                player2_score,
+                "(" + datetime + ")",
+            )
+        )
+        self.players[player1].match_count += 1
+        self.players[player2].match_count += 1
+
+        winner, loser = (
+            (player1, player2)
+            if player1_score > player2_score
+            else (player2, player1)
+        )
+        draw = player1_score == player2_score
+        winner_rating, loser_rating = trueskill.rate_1vs1(
+            trueskill.Rating(self.players[winner].rank),
+            trueskill.Rating(self.players[loser].rank),
+            drawn=draw,
+        )
+        self.players[winner].rank = int(winner_rating.mu)
+        self.players[loser].rank = int(loser_rating.mu)
+        print(winner, loser, winner_rating, loser_rating, draw)
+        self.to_json()
